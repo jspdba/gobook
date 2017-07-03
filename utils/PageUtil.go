aed9258296242da2de9773debfa48bcd19053abb
jspdba <jspdba@163.com>
2017-07-03 09:35:55 +0800

    收藏增加搜索功能

--- utils/PageUtil.go
+++ utils/PageUtil.go
@@ -8,14 +8,18 @@ type Page struct {
 	FirstPage  bool
 	LastPage   bool
 	List       interface{}
+	Key        string
+	Word       string
 }
+
 func PageUtil(count int, pageNo int, pageSize int, list interface{}) Page {
 	tp := count / pageSize
-	if count % pageSize > 0 {
-		tp = count / pageSize + 1
+	if count%pageSize > 0 {
+		tp = count/pageSize + 1
 	}
 	return Page{PageNo: pageNo, PageSize: pageSize, TotalPage: tp, TotalCount: count, FirstPage: pageNo == 1, LastPage: pageNo == tp, List: list}
 }
+
 /*func PageUser(p int, size int) utils.Page {
 	o := orm.NewOrm()
 	var user User
